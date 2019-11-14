41502348053a4da163941ac3145d955941a1cb7d
John Delpizzo <jdelpizz@citadel.edu>
2019-11-14 11:39:01 -0500

    edited test

--- src/app/Test.go
+++ src/app/Test.go
@@ -15,12 +15,13 @@ func main() {
 	fmt.Println("map:", m)
 
 	var team reality.Team = reality.TeamCreate("Red")
-	var name string = "test1"
+	var name string = "Bunker test 1"
 	var ipaddr string = "192.168.0.1"
 	var ipport int = 4000
 	var baseBunker reality.Base = reality.BaseCreate(team, name, reality.BaseBUNKER, ipaddr, ipport)
 	//sanity check
-	println(reality.BaseBUNKER)
-	println("baseBunker: %s ", reality.BaseTostring(baseBunker))
-	println(team.Name)
+	println("\nTeam: \n", team.Name)
+	println("Bunkers: \n")
+	println(reality.BaseTostring(baseBunker))
+
 }
