a89c2ece7631e7f963659caa8f794cc100a270f9
John Delpizzo <jdelpizz@citadel.edu>
2020-01-30 11:18:41 -0500

    Add hq to team, add hq to team.bases

--- lib/reality/Team.go
+++ lib/reality/Team.go
@@ -2,19 +2,35 @@ package reality
 
 //Team : Contains all the infrastructure information for each team
 type Team struct {
-	name        string
-	hq          Base
-	bases       []Base
-	baseCounter []int //uses the constant name -> int relation for index of array
+	name  string
+	hq    Base
+	bases map[string]Base
 }
 
 //TeamCreate : creates a new team and assets required
 func TeamCreate(n string) Team {
-	t := Team{name: n}
+	t := Team{name: n, bases: make(map[string]Base)}
 	return t
 }
 
 // Name : returns the name of the Team
 func (t Team) Name() string {
 	return t.name
+}
+
+// HQ : returns the HQ of the Team
+func (t Team) HQ() Base {
+	return t.hq
+}
+
+// Bases : returns all bases on the team
+func (t Team) Bases() map[string]Base {
+	return t.bases
+}
+
+// SetHQ : Sets a base as the Head Quarters
+func (t *Team) SetHQ(h Base) {
+	t.hq = h
+	t.bases[h.Name()] = h
+
 }
--- tests/Unit_Tests/Reality/team_test.go
+++ tests/Unit_Tests/Reality/team_test.go
@@ -13,6 +13,24 @@ func TestTeam(t *testing.T) {
 	if team.Name() != "Red" {
 		t.Errorf("Team name is not Red, instead '%s'", team.Name())
 	}
+	// Add Hq to team
+	println("Creating Base HQ")
+	var name string = "Red HQ"
+	var ipaddr string = "127.0.0.1"
+	var ipport int = 4000
+	var basehq reality.Base = reality.BaseCreate(team, name, reality.BaseBUNKER, ipaddr, ipport)
+	println("Adding to Team")
+	team.SetHQ(basehq)
+
+	if team.HQ().Name() != "Red HQ" {
+		t.Errorf("Team HQ is not Red HQ, instead '%s'", basehq.Name())
+	}
+	// Is HQ in the team roster?
+	println("on roster?")
+	if team.Bases()["Red HQ"] != basehq {
+		t.Errorf("Red HQ is not on the team roster")
+	}
+
 	// Add a base to a team
 
 	// Add a drone to the team
